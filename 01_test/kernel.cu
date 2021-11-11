ed1a2aae79f0b20f5695db1fcff7b268065c8913
LuisAlexisSalazar <kirito_SWO@outlook.com>
2021-11-11 14:25:16 -0500

    test

---
+++ 01_test/kernel.cu
@@ -0,0 +1,14 @@
+﻿#include <cuda_runtime.h>
+
+// ctrl+shift+space to see parameters 
+//Formatear code ctr+k ctrl+d
+//Scroll barra es información general
+int main() {
+	int* a;
+	cudaMalloc(&a, 100);
+
+	cudaFree(a);
+
+	return 0;
+
+}
